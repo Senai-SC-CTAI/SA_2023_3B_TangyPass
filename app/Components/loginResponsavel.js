d3e378ee0c502b869026cbd3ffe5d8baa4c6db78
Anselmo5 <anselmohenrique191@gmail.com>
2023-07-06 12:50:09 -0300

    alteração

--- app/Components/loginResponsavel.js
+++ app/Components/loginResponsavel.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
-
+import {Link} from 'expo-router'
 import { styles } from './styles';
 
 export function loginResponsavel() {
   return (
     <View style={styles.container}>
-
+      
     </View>
   );
 }
