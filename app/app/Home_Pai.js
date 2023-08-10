91101312aa85419698402887b2833ca1ae5b803e
Arthur <arthurrochadsg@gmail.com>
2023-08-10 11:40:32 -0300

    m

--- app/app/Home_Pai.js
+++ app/app/Home_Pai.js
@@ -111,7 +111,11 @@ const styles = StyleSheet.create({
     backgroundColor:"black",
     borderRadius:5,
   },
+  qrtxt:{
 
-
+  },
+  fontFamily:"Alata_Regular",
+  fontSize:18,
+  color:"white"
 
 });
