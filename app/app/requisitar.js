2d94cb5dfdeb4ed7b6810782a86e83a3a832e183
Anselmo5 <anselmohenrique191@gmail.com>
2023-08-18 13:00:37 -0300

    aleração

--- app/app/requisitar.js
+++ app/app/requisitar.js
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
 import React from 'react';
 import { Image, TextInput, TouchableOpacity, StyleSheet, Text, View, Button } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
 import Logo from './Logo'
 import { Alata_400Regular, useFonts } from '@expo-google-fonts/alata';
+import { CalendarOutlined, ClockCircleOutlined } from '@ant-design/icons';
 
 
 export default function Requisitar() {
@@ -14,34 +14,45 @@ export default function Requisitar() {
 
   if (!fontsLoaded) {
     return null;
-  } 
- 
+  }
+
   return (
     <View style={styles.container}>
-        <Logo style={styles.aling} />
-        <Text style={styles.Rsd}>Requisitar Saida</Text>
+      <Logo style={styles.aling} />
+      <Text style={styles.Rsd}>Requisitar Saida</Text>
 
       <View style={styles.subcontainer}>
-       <View style={styles.introductiontext}>
-          <TextInput style={styles.formats} placeholder='Selecionar Dia'>
+        <View style={styles.introductiontext}>
 
-          </TextInput>
+          <View style={styles.alinginpt} >
 
-           <TextInput style={styles.formats} placeholder='Selecionar Hora' name='calendar' >
-           
-          </TextInput>
-      </View >
-      <View style={styles.subcontaineredunc}>
+            <CalendarOutlined style={styles.icon} />
+            <TextInput style={styles.formats} placeholder='Selecionar Dia'>
 
-        <TouchableOpacity style={styles.formatspress}>
+            </TextInput>
+
+          </View>
+          <View style={styles.alinginpt}>
+
+            <ClockCircleOutlined />
+            <TextInput style={styles.formats} placeholder='Selecionar Hora' name='calendar' >
+
+            </TextInput>
+          </View>
+
+        </View >
+
+        <View style={styles.subcontaineredunc}>
+
+          <TouchableOpacity style={styles.formatspress}>
             <View style={styles.PressText}>
-                Requisitar Saida
+              Requisitar Saida
             </View>
           </TouchableOpacity>
 
-          <Link href={'Home_Estudante'} style={styles.Voltade}>Voltar</Link>  
+          <Link href={'Home_Estudante'} style={styles.Voltade}>Voltar</Link>
         </View>
-      </View> 
+      </View>
     </View>
   );
 }
@@ -51,72 +62,82 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#f6f6f6',
     alignItems: 'center',
-  },
-
-  subcontainer:{
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    flexDirection:'column',
-    marginTop:180,
+    justifyContent: 'center',
 
   },
-  Rsd:{
-    textAlign:'center',
-    fontSize:"1em",
-    fontFamily:"Alata_400Regular",
-    marginTop:10,
+
+  subcontainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    marginTop: 180,
+
+  },
+  Rsd: {
+    fontSize: "1em",
+    fontFamily: "Alata_400Regular",
+    marginTop: 10,
   },
 
-  introductiontext:{
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'space-between',
-    flexDirection:'row',
+  introductiontext: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
     width: "100%",
     maxWidth: 300,
+ 
   },
 
-  formats:{
-    padding:15,
-    borderRadius:5,
-    width:"100%",
-    margin:5,
-    backgroundColor:'#fffff',
+  formats: {
+    padding: 15,
+    borderRadius: 5,
+    width: "60%",
+    margin: 5,
+    backgroundColor: '#fffff',
     shadowOffset: { width: 5, height: 5 },
     shadowColor: '#000010',
     shadowOpacity: 0.6,
-    shadowRadius:5,
-    color:'#BFBFBF',
-    textAlign:'center',
-    fontFamily:"Alata_400Regular",
+    shadowRadius: 5,
+    color: '#BFBFBF',
+    fontFamily: "Alata_400Regular",
   },
-  subcontaineredunc:{
-    display:'flex',
-    alignItems:'center',
-    justifyContent:'center',
-    flexDirection:'column',
-    width:"calc(100% - 10px)",
-    maxWidth: 300
+  subcontaineredunc: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    width: "calc(100% - 10px)",
+    maxWidth: 300,
   },
 
-  formatspress:{
-    width:"100%",
-    margin:5,
-    padding:10,
-    borderRadius:5,
-    backgroundColor:'black',
+  formatspress: {
+    width: "100%",
+    margin: 5,
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: 'black',
   },
-  PressText:{
-    color:'#fff',
-    textAlign:'center',
-    fontFamily:"Alata_400Regular",
+  PressText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontFamily: "Alata_400Regular",
   },
-  Voltade:{
-    textAlign:'center',
-    fontSize:"1em",
-    fontFamily:"Alata_400Regular",
-    marginTop:100,
+  Voltade: {
+    textAlign: 'center',
+    fontSize: "1em",
+    fontFamily: "Alata_400Regular",
+    marginTop: 100,
   },
-  
+  icon: {
+    marginRight:180,
+  },
+  // alinginpt: {
+  //   display: 'flex',
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  //   flexDirection: "column"
+  // }
+
 });
