c2ab24661424e4bfc6bbe656058c5d804e607707
Anselmo5 <anselmohenrique191@gmail.com>
2023-10-02 21:08:09 -0300

    update

--- web/TanyPass/src/components/index.jsx
+++ web/TanyPass/src/components/index.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
- import './index.css'
+import './index.css'
+import logo from '../assets/Logo.png'
+import college from '../assets/college project-pana (1) 1.png'
 const index = () => {
   return (
-    <div>   
-         
+    <div>
 
+      <div className='aling'>
+        <div className='form1'>
+          <div className='logo'>
+            <img src={logo} alt="" />
+          </div>
+          <div className='text'>
+            <h2>Faça Login Para Começarmos Sua experiência</h2>
+          </div>
+
+          <div className='logo2'>
+            <img src={college} alt="" />
+          </div>
+        </div>
+
+
+        <div className='form2'>
+          <div className='text2'>
+            <h2>Login</h2>
+          </div>
+
+
+
+
+          <div className='formulario'>
+            <input type="text" className='inp' placeholder='Usuário( Email )' />
+            <input type="email" className='inp' placeholder='Insira sua senha' />
+            <button className='btn'>Enviar</button>
+            <a href="" className='yper'> Esqueceu a senha? </a>
+            <p className='textfooter'>Tangy.app @2023</p>
+          </div>
+
+        </div>
+      </div>
 
     </div>
   )
